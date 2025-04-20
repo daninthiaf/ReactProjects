399470831fada4f46d1a5d8843c2e6161531b96e
Daninthia Fox <daninthia@Daninthias-MacBook-Air.local>
2025-04-20 19:47:03 -0400

    Full functionality of the calculator complete. May require some bug fixes.

--- src/DigitButton.js
+++ src/DigitButton.js
@@ -1,10 +1,14 @@
 import { ACTIONS } from './App';
-import DigitButton from './DigitButton';
+//import DigitButton from './DigitButton';
 
 export default function DigitButton({dispatch, digit}) {
     return(
         <button 
-        onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}>{digit}
+        onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+        >
+            
+            {digit}
         
         </button>
+    )
 }
